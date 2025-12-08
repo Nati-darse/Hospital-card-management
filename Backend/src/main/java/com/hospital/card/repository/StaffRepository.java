7f59cbe37b504547167b0606dd13e3d5b4807313
Nati-darse <natnaeldarsema@gmail.com>
2025-12-08 17:07:02 +0300

    Add StaffRepository (JpaRepository)

---
+++ Backend/src/main/java/com/hospital/card/repository/StaffRepository.java
@@ -0,0 +1,9 @@
+package com.hospital.card.repository;
+
+import com.hospital.card.entity.Staff;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StaffRepository extends JpaRepository<Staff, Long> {
+}
