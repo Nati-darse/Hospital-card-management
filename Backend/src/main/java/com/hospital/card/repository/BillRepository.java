d68477b1dcd7551e4462a4548ca1a5e39fd2643c
Nati-darse <natnaeldarsema@gmail.com>
2025-12-08 17:07:00 +0300

    Add BillRepository (JpaRepository)

---
+++ Backend/src/main/java/com/hospital/card/repository/BillRepository.java
@@ -0,0 +1,9 @@
+package com.hospital.card.repository;
+
+import com.hospital.card.entity.Bill;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BillRepository extends JpaRepository<Bill, Long> {
+}
