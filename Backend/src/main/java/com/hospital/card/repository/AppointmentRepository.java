a78f93565e1ed8bd6f664ba64165ddd4a7a8b601
Nati-darse <natnaeldarsema@gmail.com>
2025-12-08 17:07:00 +0300

    Add AppointmentRepository (JpaRepository)

---
+++ Backend/src/main/java/com/hospital/card/repository/AppointmentRepository.java
@@ -0,0 +1,9 @@
+package com.hospital.card.repository;
+
+import com.hospital.card.entity.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+}
