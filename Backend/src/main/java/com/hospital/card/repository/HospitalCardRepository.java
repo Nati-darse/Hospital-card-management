7dee2267480597e2cb128ad537e62faba63efbc9
Nati-darse <natnaeldarsema@gmail.com>
2025-12-08 17:07:00 +0300

    Add HospitalCardRepository (JpaRepository)

---
+++ Backend/src/main/java/com/hospital/card/repository/HospitalCardRepository.java
@@ -0,0 +1,12 @@
+package com.hospital.card.repository;
+
+import com.hospital.card.entity.HospitalCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface HospitalCardRepository extends JpaRepository<HospitalCard, Long> {
+    Optional<HospitalCard> findByCardNumber(String cardNumber);
+}
